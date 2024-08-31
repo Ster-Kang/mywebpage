c021e97b6e909b78e51d689eafb0add74fa933fa
Ster Kang <ster.ksw.kang@mgmail.com>
2024-09-01 00:06:37 +0900

    함수형으로 구조 변경 및 props 할당 연습

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+type AppProps = {
+  title?: string;
+  subtitle?: string;
+  isVisible?: boolean;
+}
+
+const App = (
+  { 
+    title = "기본 타이틀", 
+    subtitle = "기본 서브타이틀", 
+    isVisible = true
+  }:AppProps): JSX.Element => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{title}</h1>
+      <h2>{subtitle}</h2>
+      {isVisible && <p>This is visible</p>}
     </div>
   );
-}
+};
 
 export default App;
